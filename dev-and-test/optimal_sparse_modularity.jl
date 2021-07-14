943f8e13147f6135500e82bbc3d24c21aa4e9f10
David Gleich <dgleich@purdue.edu>
2021-07-14 09:44:35 -0400

    New codes to test direct Gurobi vs. Jump for optimal temporal modularity

--- dev-and-test/optimal_sparse_modularity.jl
+++ dev-and-test/optimal_sparse_modularity.jl
@@ -26,7 +26,7 @@ function badtris(A::SparseMatrixCSC)
                         parity = nz[nzi]*nz[nzi2]*nz[neighind[k]]
                         if parity < 0
                             if nz[nzi] < 0 && nz[nzi2] < 0 && nz[neighind[k]] < 0
-                                # --- isn't a bad triangle... it's okay... 
+                                # --- isn't a bad triangle... it's okay...
                             else
                                 push!(tris, (i,j,k))
                             end
@@ -44,6 +44,133 @@ function badtris(A::SparseMatrixCSC)
     end
     println("$(length(tris)) bad triangles")
     return tris
+end
+
+
+function LazyExactSparseModularityGurobi(B,outputflag = true,verbose = true)
+    W = copy(B)
+    n = size(W,1)
+
+    edgevals = collect(zip(findnz(M)[1:3]...))
+
+
+    vmap = -ones(Int,n,n)
+    obj = Float64[]
+    nvars = 0
+    for j=1:n
+        for i=1:j-1
+            nvars += 1
+            vmap[i,j] = nvars-1
+            vmap[j,i] = nvars-1
+            push!(obj, W[i,j])
+        end
+    end
+    vtypes = repeat(GRB_BINARY, nvars)
+    aptr = Ref{Ptr{Cvoid}}()
+    err = GRBnewmodel(gurobi_env, aptr, "LazyLambdaCC", nvars, obj, C_NULL, C_NULL, vtypes, C_NULL)
+    m = aptr[]
+
+    try
+        cind = Int32[0,0,0]
+        cval = Float64[0,0,0]
+
+        println()
+        println("Adding bad triangles...")
+        btris = badtris(W)
+        for (i,j,k) in btris
+            cind[1] = vmap[j,k]
+            cind[2] = vmap[i,k]
+            cind[3] = vmap[i,j]
+            cval[1] = 1
+            cval[2] = -1
+            cval[3] = -1
+            error = GRBaddconstr(m, 3, cind, cval, GRB_LESS_EQUAL, 0.0, C_NULL)
+        end
+
+        # Find intial first solution
+        if verbose
+            println("First round of optimization")
+        end
+
+        GRBoptimize(m)
+        robj = Ref{Float64}(0.0)
+        GRBgetdblattr(m, GRB_DBL_ATTR_OBJVAL, robj)
+        obj = robj[]
+        soln = zeros(nvars)
+        GRBgetdblattrarray(m, GRB_DBL_ATTR_X, 0, nvars, soln)
+        D = zeros(n,n)
+        for j=1:n
+            for i=1:j-1
+                D[i,j] = soln[vmap[i,j]+1]
+                D[j,i] = soln[vmap[i,j]+1]
+            end
+        end
+
+        while true
+            # x will naturally be upper triangular, but 'find_violations'  wants lower triangular
+              #D = Matrix(JuMP.value.(x)')
+
+             # Store violating tuples in a vector
+             violations = Vector{Tuple{Int,Int,Int}}()
+
+             find_violations!(D,violations)
+
+             # Iterate through and add constraints
+             numvi = size(violations,1)
+             if verbose
+                print("Adding in $numvi violated constraints.")
+             end
+
+             # Violations (a,b,c) are ordered so that a < b, and (a,b) is the value
+             # that needs to be positive in the inequality:
+             # x_ab - x_ac - x_bc <= 0.
+             # The other two (b and c) could satisfy either b < c or c <= b.
+             # We need to make sure we are only placing constraints in the upper
+             # triangular portion of the matrix.
+             # We do so by just calling min and max on pairs of nodes
+             for v in violations
+                 #assert(v[1]<v[2])
+                 cind[1] = vmap[v[1],v[2]]
+                 cind[2] = vmap[min(v[1],v[3]),max(v[1],v[3])]
+                 cind[3] = vmap[min(v[2],v[3]),max(v[2],v[3])]
+                 cval[1] = 1
+                 cval[2] = -1
+                 cval[3] = -1
+                 #@show cind, cval
+                 error = GRBaddconstr(m, 3, cind, cval, GRB_LESS_EQUAL, 0.0, C_NULL)
+                 #z = sparsevec(cind.+1, cval, nvars)
+                 #@show z'*soln
+                 #@constraint(m,x[v[1],v[2]] - x[min(v[1],v[3]),max(v[1],v[3])] - x[min(v[2],v[3]),max(v[2],v[3])] <= 0)
+             end
+             if numvi == 0
+                 if verbose
+                    println(" Optimal solution found.")
+                 end
+                 break
+             end
+             if verbose
+                println(" And re-solving the ILP.")
+             end
+             GRBupdatemodel(m)
+             err = GRBoptimize(m)
+             #@show err
+             robj = Ref{Float64}(0.0)
+             GRBgetdblattr(m, GRB_DBL_ATTR_OBJVAL, robj)
+             obj = robj[]
+             #@show "obj after solve", obj
+             GRBgetdblattrarray(m, GRB_DBL_ATTR_X, 0, nvars, soln)
+             for j=1:n
+                 for i=1:j-1
+                     D[i,j] = soln[vmap[i,j]+1]
+                     D[j,i] = soln[vmap[i,j]+1]
+                 end
+             end
+         end
+         return extractClustering(D), D
+    finally
+        #@show "freemodel here!"
+        GRBfreemodel(m)
+    end
 end
 
 
@@ -144,7 +271,7 @@ end
 
 function exact_temporal_modularity(B)
     #m = sum(nonzeros(A))/2
-    c, D,bound =  LazyExactSparseModularity(B)
+    c, D,bound =  LazyExactSparseModularityGurobi(B)
 end
 
 Tcc_ids = vec([ 672 728 729 730 733 734 826 735 737 751 792 818 133 80 81 83 88 102 123 124 125 79 137 140 159 161 162 220 229 34 2 3 4 5 7 22 30 32 239 35 36 48 54 67 68 77 536 551 556 559 576 584 508 613 635 639 666 1 677 694 350 241 245 276 278 292 293 294 299 368 375 399 412 480 498 503 795])
