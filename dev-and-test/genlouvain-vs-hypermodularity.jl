8128caf68c2879a064b1d78c09305c7847c00c80
David Gleich <dgleich@purdue.edu>
2021-07-19 09:22:59 -0400

    Small changes to testing and development scripts

--- dev-and-test/genlouvain-vs-hypermodularity.jl
+++ dev-and-test/genlouvain-vs-hypermodularity.jl
@@ -1,6 +1,8 @@
 ## Test HyperMoularity.LambdaLouvain with implicit vs. explicit graphs...
-# 
-include("methods.jl")
+# Conclusion -- the raw LambdaLouvain call on the temporal modularity
+# matrix directly is equivalent to another implementation of Louvain clustering.
+
+include("../methods.jl")
 G = _build_email_tofrom_graph(data;keepfauci=false,mindegree=2)
 ##
 A = G.A
@@ -8,49 +10,42 @@ A = G.A
 using HyperModularity
 cc = HyperModularity.VanillaModularity(Float64.(A),1.0)
 ##
+include("../include/Optimal_LambdaCC.jl")
+##
 # k = vec(sum(G.A,dims=2))
 # B = sparse(A - (2/sum(A))*(k*k'))
 # ccs = HyperModularity.LambdaLouvain(Float64.(B),zeros(size(B,1)),0.0)
-##
-ccs[:,end]
-##
-function HyperModularity.ConstructAdj(C::SparseArrays.SparseMatrixCSC,n::Int64)
-    """
-    ConstructAdj: Construct Adjacency List
-    This takes in a sparse adjacency matrix for a graph, and returns an adjacency
-    list. While it seems inefficient to store the matrix in multiple ways, as long
-    as there are no terrible memory issues, it is VERY handy to be able to quickly
-    get a list of neighbors for each node.
-
-    The function also returns the degree vector d. This is NOT the weighted degree,
-        d[i] = total number of neighbors of node i
-    """
-
-    @show "minenew!"
-    rv = C.rowval
-    cp = C.colptr
-    nz = C.nzval
-    Neighbs = Vector{Vector{Int64}}()
-    d = zeros(Int64,n)
-    for i = 1:n
-        # chop up the rp vector and put it in Neighbs
-        neighs = eltype(rv)[]
-        for nzi=cp[i]:cp[i+1]-1
-            if nz[nzi] > 0
-                push!(neighs, rv[nzi])
-            end
-        end
-        push!(Neighbs,neighs)
-        d[i] = length(neighs)
-    end
-
-    # d is the number of neighbors. This is the unweighted degree,
-    # but note importantly that if the original graph is weighted this is
-    # not the same as the degree vector d we will sometimes use
-    return Neighbs, d
-end
 
 ##
 k = vec(sum(G.A,dims=2))
 B = sparse(A - (1/sum(A))*(k*k'))
 ccs = HyperModularity.LambdaLouvain(Float64.(B),zeros(size(B,1)),0.0)
+
+##
+m1 = compute_modularity(A, vec(sum(A;dims=2)), cc[:,end])
+##
+function compute_modularity(A::SparseMatrixCSC,c)
+    obj = 0
+    m = sum(nonzeros(A)/2)
+    n = size(A,1)
+    # index c...
+    C = sparse(1:n, c, 1, n, maximum(c))
+    Crv = rowvals(C)
+    rv = rowvals(A)
+    nz = nonzeros(A)
+    for ci=1:size(C,2)
+      Cset = Set(Crv[nzrange(C,ci)]) # index the cluster
+      for nzci in nzrange(C,ci)
+        i = Crv[nzci]
+        for nzi in nzrange(A,i)
+          if rv[nzi] in Cset
+            obj += nz[nzi]
+          end
+        end
+      end
+    end
+    return obj
+end
+m2 = compute_modularity(B, cc[:,end])/sum(A)
+##
+m3 = compute_modularity(B, ccs[:,end])/sum(A)
